5d6d9b1698fc033093b50bf087df67da7ef3f7f1
Brandon Mehelal <brandon.mehelal@gmail.com>
2024-04-21 19:00:22 -0400

    Deleted semi-colon

--- server/src/main/java/com/harith/ecommerce/Repository/UserRepository.java
+++ server/src/main/java/com/harith/ecommerce/Repository/UserRepository.java
@@ -2,7 +2,8 @@ package com.harith.ecommerce.Repository;
 
 import com.harith.ecommerce.Model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.Optional;;
+import java.util.Optional;
+
 // Gives us preDefined methods that allow us to interact with our database with ease, specifcally for our User table
 public interface UserRepository extends MongoRepository<User, String> {
     Optional<User> findByUsername(String username);
