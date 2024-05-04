51db31f1f4e37ed5692288f1935c8342fa78fc2f
Brandon Mehelal <brandon.mehelal@gmail.com>
2024-05-03 22:38:42 -0400

    Working on the checkout logic, still not done yet

--- client/ecommerce/src/pages/shop/product.tsx
+++ client/ecommerce/src/pages/shop/product.tsx
@@ -1,4 +1,6 @@
+import { useContext } from "react";
 import { IProduct } from "../../models/interfaces";
+import { IShopContext, ShopContext } from "../../context/shop-context";
 
 interface Props {
   product: IProduct;
@@ -14,7 +16,9 @@ export const Product = (props: Props) => {
     vocation,
     stockQuanity,
   } = props.product;
-// styling would be turn all of these into cards
+  const { addToCart, getCartItemCount } = useContext<IShopContext>(ShopContext);
+  const cartItemCount: number = getCartItemCount(productId);
+  // styling would be turn all of these into cards
   return (
     <>
       <img src={productURL} />{" "}
@@ -25,7 +29,13 @@ export const Product = (props: Props) => {
         <p>Speciality: {vocation}</p>
         <p>Seller: {sellerName}</p>
       </div>
-      <div>{stockQuanity > 0 && <button>Add To Cart</button>}</div>
+      <div>
+        {stockQuanity > 0 && (
+          <button onClick={() => addToCart(productId)}>
+            Add To Satchel {cartItemCount > 0 && <>({cartItemCount})</>}
+          </button>
+        )}
+      </div>
     </>
   );
 };
